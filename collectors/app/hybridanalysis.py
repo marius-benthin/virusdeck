779c21799712008926e75d77f83f47d360cd1fac
Marius Benthin <marius@benthin.me>
2021-03-02 21:34:17 +0100

    Added file hash collector for Hybrid Analysis

---
+++ collectors/app/hybridanalysis.py
@@ -0,0 +1,43 @@
+import re
+import json
+import logging
+from typing import List
+
+from collector import Collector
+
+
+class HybridAnalysis(Collector):
+
+    def __init__(self):
+        Collector.__init__(self, offset=5)
+
+    @staticmethod
+    def parse_feed(feed: str, pattern: re.Pattern = None) -> List:
+        """
+        Extracts file hashes from Hybrid Analysis feed.
+        :param feed: fetched feed
+        :param pattern: None, not used
+        :return: list of file hashes
+        """
+        file_hashes: List = []
+        try:
+            json_feed: json = json.loads(feed)
+            for sample in json_feed["data"]:
+                if "isurlanalysis" in sample and not sample["isurlanalysis"]:
+                    if "threatlevel" in sample and sample["threatlevel"] >= 2:
+                        if "sha256" in sample:
+                            file_hashes.append(sample["sha256"])
+                        if "md5" in sample:
+                            file_hashes.append(sample["md5"])
+        except ValueError as w:
+            logging.warning(w)
+        return file_hashes
+
+    def get_file_hashes(self) -> List:
+        """
+        Hybrid Analysis generates file hashes approximately every five minutes.
+        :return: list of file hashes
+        """
+        feed: str = self.fetch_feed("https://www.hybrid-analysis.com/feed?json",
+                                    headers={'User-Agent': 'Falcon Sandbox'})
+        return self.parse_feed(feed=feed)
