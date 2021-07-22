fcdebb72b13b3fb20de21dae430e192279620761
Marius Benthin <marius@benthin.me>
2021-07-22 21:25:44 +0200

    Added sample source IDs as model

---
+++ virusdeck/models/sample_sources.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SampleSource(Enum):
+    BAZAAR = 0
+    URLHAUS = 1
+    VIRUSSHARE = 2
+    VX_UNDERGROUND = 3
+    MALSHARE = 4
+    HYBRID_ANALYSIS = 5
+    THREATFOX = 6
