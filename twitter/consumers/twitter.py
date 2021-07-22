ec280d83960a413ad3d9b2e1c8f282582369dc83
Marius Benthin <marius@benthin.me>
2021-07-22 10:40:36 +0200

    Fixed relative imports again

--- twitter/consumers/twitter.py
+++ twitter/consumers/twitter.py
@@ -12,7 +12,7 @@ import sqlalchemy as db
 from sqlalchemy import exc
 from sqlalchemy.dialects.mysql import insert
 
-from ..consumers.subscriber import Subscriber
+from .subscriber import Subscriber
 from models.twitter_tweet import Tweet
 from models.twitter_tables import Base, TableTweet, TableUser
 
